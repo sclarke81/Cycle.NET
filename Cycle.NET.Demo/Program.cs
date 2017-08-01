c1a0a22b193101a666ee56210177e692998cdfd4
Stephen Clarke <sclarke81@gmail.com>
2017-08-01 12:08:14 +0100

    Initial version

---
+++ Cycle.NET.Demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+
+namespace Cycle.NET.Demo
+{
+    class Program
+    {
+        private static IObservable<object> LogDriver(IObservable<object> sinks)
+        {
+            sinks.Cast<int>().Subscribe(i => Console.WriteLine("Log " + i.ToString()));
+
+            return Observable.Empty<object>();
+        }
+
+        private static IObservable<object> KeyInputDriver(IObservable<object> sinks)
+        {
+            return Observable.Range(20, 5).Select(i => (object)i);
+        }
+
+        private static Streams CycleMain(Streams sources)
+        {
+            var sinks = new Streams
+            {
+                { "log", sources["keys"] }
+            };
+
+            return sinks;
+        }
+        static void Main(string[] args)
+        {
+            var drivers = new Drivers
+            {
+                { "log", LogDriver },
+                { "keys", KeyInputDriver }
+            };
+            Runner.Run(CycleMain, drivers);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Cycle.NET
+
+> .NET implementation of [Cycle.js]
+
+[Cycle.js]: https://github.com/cyclejs/cyclejs
+
+This project is in the extremely early stages of development and is not ready for use (even if some functionality is available).
+
+## Contributions and feedback
+
+Both contributions and feedback are gratefully received. I have very little experience with Cycle.js (or Javascript for that matter) but I'm interested in the Cycle.js paradigm and thought that I'd my efforts to try it out in C#.
+
+## License
+
+Everything in this repository is MIT License unless otherwise specified.
+
+MIT © Stephen Clarke.
+
+The full license can be found in [LICENSE](LICENSE).
