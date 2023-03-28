c30ec5875007664e56bf54ca6a62cd38104c68f7
Nathaniel Bond <nabond251@gmail.com>
2023-03-27 20:29:25 -0400

    Implement demo with obs of tuple

--- Cycle.NET.Demo/Program.cs
+++ Cycle.NET.Demo/Program.cs
@@ -18,24 +18,35 @@ namespace Cycle.NET.Demo
             return Observable.Range(20, 5);
         }
 
-        private static Streams<int, int, Unit> CycleMain(Streams<int, int, Unit> sources)
+        private static IObservable<(int? Log, int? KeyInput)> Driver(IObservable<(int? Log, int? KeyInput)> sinks)
         {
-            var sinks = new Streams<int, int, Unit>(
-                sources.Second,
-                Observable.Empty<int>(),
-                Observable.Empty<Unit>());
+            var logSinks = sinks.Where(s => s.Log.HasValue);
+            var logSources = LogDriver(logSinks.Select(s => s.Log.Value));
+
+            var keyInputSinks = sinks.Where(s => s.KeyInput.HasValue);
+            var keyInputSources = KeyInputDriver(keyInputSinks.Select(s => s.KeyInput.Value));
+
+            return Observable.Merge(
+                logSources.Select(s => (Log: s as int?, KeyInput: null as int?)),
+                keyInputSources.Select(s => (Log: null as int?, KeyInput: s as int?)));
+        }
+
+        private static Streams<(int? Log, int? KeyInput)> CycleMain(Streams<(int? Log, int? KeyInput)> sources)
+        {
+            var sinks = new Streams<(int? Log, int? KeyInput)>(
+                sources.First.Select(s => (
+                    Log: s.KeyInput,
+                    KeyInput: null as int?)));
 
             return sinks;
         }
         static void Main(string[] args)
         {
-            var component = new Component<int, int, int, int, Unit, Unit>(
+            var component = new Component<(int? Log, int? KeyInput), (int? Log, int? KeyInput)>(
                 CycleMain);
-            var drivers = new Drivers<int, int, int, int, Unit, Unit>(
-                onFirst: LogDriver,
-                onSecond: KeyInputDriver,
-                onThird: _ => _);
-            Runner<int, int, int, int, Unit, Unit>.Run(component, drivers);
+            var drivers = new Drivers<(int? Log, int? KeyInput), (int? Log, int? KeyInput)>(
+                onFirst: Driver);
+            Runner<(int? Log, int? KeyInput), (int? Log, int? KeyInput)>.Run(component, drivers);
             Console.ReadLine();
         }
     }
