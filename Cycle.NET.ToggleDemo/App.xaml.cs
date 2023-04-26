f79ec21f2d69c3ae322205ab58f812743b96ae04
Nathaniel Bond <nabond251@gmail.com>
2023-04-26 00:54:04 -0400

    Add demo app project

---
+++ Cycle.NET.ToggleDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Cycle.NET.ToggleDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Cycle.NET.ToggleDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cycle.NET.ToggleDemo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
